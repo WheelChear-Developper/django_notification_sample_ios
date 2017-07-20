fc137bc2aaf23b5d74a6c881f78f6ebe74a36f2f
M.Amatani <amatani@mobile-innovation.jp>
2017-07-20 18:41:43 +0900

    first commit

--- django-notification/AppDelegate.m
+++ django-notification/AppDelegate.m
@@ -16,7 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    // push通知呼び出し用
+    UIUserNotificationType types =    UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [application registerUserNotificationSettings:mySettings];
+
     return YES;
 }
 
@@ -47,5 +52,105 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+////////////////// デバイストークン関連 //////////////////////
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    [application registerForRemoteNotifications];
+}
+
+// デバイストークン取得成功
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+    // デバイストークンの両端の「<>」を取り除く
+    NSString *deviceTokenString = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+
+    // デバイストークン中の半角スペースを除去する
+    deviceTokenString = [deviceTokenString stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"Device Token = %@",deviceTokenString);
+//    [Configuration setDeviceTokenKey:deviceTokenString];
+
+#ifdef LOCAL
+    //LOCAL
+    NSLog(@"▶LOCAL:%@",deviceTokenString);
+    /*
+     //トースト
+     [self.window.rootViewController.view makeToast:[NSString stringWithFormat:@"[ %@ ]",deviceTokenString]
+     duration:5.0
+     position:CSToastPositionBottom
+     title:@"> LOCAL"
+     image:nil
+     style:nil
+     completion:^(BOOL didTap) {
+     }];
+     */
+#endif
+#ifdef STAGING
+    //Rease
+    NSLog(@"▶STAGING:%@",deviceTokenString);
+    /*
+     //トースト
+     [self.window.rootViewController.view makeToast:[NSString stringWithFormat:@"[ %@ ]",deviceTokenString]
+     duration:5.0
+     position:CSToastPositionBottom
+     title:@"> STAGING"
+     image:nil
+     style:nil
+     completion:^(BOOL didTap) {
+     }];
+     */
+#endif
+#ifdef PRODUCTION
+    //Rease
+    NSLog(@"▶PRODUCTION:%@",deviceTokenString);
+    /*
+     //トースト
+     [self.window.rootViewController.view makeToast:[NSString stringWithFormat:@"[ %@ ]",deviceTokenString]
+     duration:5.0
+     position:CSToastPositionBottom
+     title:@"> PRODUCTION"
+     image:nil
+     style:nil
+     completion:^(BOOL didTap) {
+     }];
+     */
+#endif
+}
+
+// デバイストークン取得失敗
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)err
+{
+    NSLog(@"Error in registration: %@", err);
+    UIAlertController *alert =
+    [UIAlertController alertControllerWithTitle:@"認証エラー"
+                                        message:[NSString stringWithFormat:@"ErrorCode: %ld", (long)[err code]]
+                                 preferredStyle:UIAlertControllerStyleAlert];
+
+    [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Dialog_Ok",@"") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self tokenErr_okPush];
+    }]];
+
+    [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithError:(NSError *)err
+{
+    NSLog(@"didRegisterForRemoteNotificationsWithError; ErrorCode: %ld", (long)[err code]);
+}
+
+// アラートのボタンが押された時に呼ばれる
+- (void)tokenErr_okPush {
+
+#ifdef DEBUG
+
+#endif
+#ifdef STAGING
+//    exit(0);
+#endif
+#ifdef PRODUCTION
+//    exit(0);
+#endif
+}
+////////////////// デバイストークン関連 //////////////////////
 
 @end
